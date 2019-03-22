3bb92fa59c54f498f394ed7f6a82949cf759a27a
Diego <diego_henriquerodrigues@hotmail.com>
2019-03-22 11:55:52 -0300

    Projeção dos dados do autor com DTO

--- src/main/java/br/com/agilworks/springmongo/config/Instantiation.java
+++ src/main/java/br/com/agilworks/springmongo/config/Instantiation.java
@@ -2,6 +2,7 @@ package br.com.agilworks.springmongo.config;
 
 import br.com.agilworks.springmongo.domain.Post;
 import br.com.agilworks.springmongo.domain.User;
+import br.com.agilworks.springmongo.dto.AuthorDTO;
 import br.com.agilworks.springmongo.repository.PostRepository;
 import br.com.agilworks.springmongo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,10 +33,11 @@ public class Instantiation implements CommandLineRunner {
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
-        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para são paulo abraços!", maria);
-        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia","Acordei feliz hoje!", maria);
-
         userReposiroty.saveAll(Arrays.asList(maria, alex, bob));
+
+        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para são paulo abraços!", new AuthorDTO(maria));
+        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia","Acordei feliz hoje!", new AuthorDTO(maria));
+
         postRepository.saveAll(Arrays.asList(post1, post2));
     }
 }
