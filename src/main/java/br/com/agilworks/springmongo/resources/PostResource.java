1be50c13e3c1f6290e9ff6fa9926ef02263bed7c
Diego <diego_henriquerodrigues@hotmail.com>
2019-03-22 14:47:12 -0300

    Consulta simples com quer methods

--- src/main/java/br/com/agilworks/springmongo/resources/PostResource.java
+++ src/main/java/br/com/agilworks/springmongo/resources/PostResource.java
@@ -1,10 +1,13 @@
 package br.com.agilworks.springmongo.resources;
 
 import br.com.agilworks.springmongo.domain.Post;
+import br.com.agilworks.springmongo.resources.util.URL;
 import br.com.agilworks.springmongo.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value="/posts")
@@ -17,5 +20,14 @@ public class PostResource {
         Post obj = postService.findById(id);
 
         return ResponseEntity.ok().body(obj);
+    }
+
+    @GetMapping(value="/titlesearch")
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue = "") String text) {
+        text = URL.decodeParam(text);
+
+        List<Post> list = postService.findByTitle(text);
+
+        return ResponseEntity.ok().body(list);
     }
 }
