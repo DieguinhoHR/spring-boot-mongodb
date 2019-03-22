261f84ea27ca75a64b687cdc12ad2440a29b4177
Diego <diego_henriquerodrigues@hotmail.com>
2019-03-22 14:15:30 -0300

    Acrescentando comentários aos posts

--- src/main/java/br/com/agilworks/springmongo/config/Instantiation.java
+++ src/main/java/br/com/agilworks/springmongo/config/Instantiation.java
@@ -3,6 +3,7 @@ package br.com.agilworks.springmongo.config;
 import br.com.agilworks.springmongo.domain.Post;
 import br.com.agilworks.springmongo.domain.User;
 import br.com.agilworks.springmongo.dto.AuthorDTO;
+import br.com.agilworks.springmongo.dto.CommentDTO;
 import br.com.agilworks.springmongo.repository.PostRepository;
 import br.com.agilworks.springmongo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -37,6 +38,13 @@ public class Instantiation implements CommandLineRunner {
 
         Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para são paulo abraços!", new AuthorDTO(maria));
         Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia","Acordei feliz hoje!", new AuthorDTO(maria));
+
+        CommentDTO c1 = new CommentDTO("Boa viagem mano", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+        CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+        CommentDTO c3 = new CommentDTO("tenha um ótimo dia", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+
+        post1.getComments().addAll(Arrays.asList(c1, c2));
+        post2.getComments().addAll(Arrays.asList(c1, c2, c3));
 
         postRepository.saveAll(Arrays.asList(post1, post2));
 
