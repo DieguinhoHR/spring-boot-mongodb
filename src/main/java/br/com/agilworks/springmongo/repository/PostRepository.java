656d51977d518e18161eb32c96123805d0194e78
Diego <diego_henriquerodrigues@hotmail.com>
2019-03-22 14:59:44 -0300

    Consulta simples com @Query

--- src/main/java/br/com/agilworks/springmongo/repository/PostRepository.java
+++ src/main/java/br/com/agilworks/springmongo/repository/PostRepository.java
@@ -2,12 +2,16 @@ package br.com.agilworks.springmongo.repository;
 
 import br.com.agilworks.springmongo.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+    // @Query
+    @Query("{ 'title': { $regex: ?0, $options: 'i' } }") // i pra ignorar maiúsculas e minúsculas
+    List<Post> searchTitle(String search);
     // @Query methods
     List<Post> findByTitleContainingIgnoreCase(String text);
 }
