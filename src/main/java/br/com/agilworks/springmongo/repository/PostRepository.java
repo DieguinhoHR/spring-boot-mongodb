e30b0b28299c3c192d349b6761d59975f7a392e3
Diego <diego_henriquerodrigues@hotmail.com>
2019-03-22 11:44:41 -0300

    Criando entity post

---
+++ src/main/java/br/com/agilworks/springmongo/repository/PostRepository.java
@@ -0,0 +1,9 @@
+package br.com.agilworks.springmongo.repository;
+
+import br.com.agilworks.springmongo.domain.Post;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+}
